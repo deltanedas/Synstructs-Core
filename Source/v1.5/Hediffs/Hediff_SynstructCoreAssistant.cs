a03d26a168774509e3e3249378ff12122c4ad7bd
Killathon <huelva21@gmail.com>
2024-06-07 20:41:30 -0400

    Additional research learning letters, core assistants lost on core destruction

--- Source/v1.5/Hediffs/Hediff_SynstructCoreAssistant.cs
+++ Source/v1.5/Hediffs/Hediff_SynstructCoreAssistant.cs
@@ -1,9 +1,23 @@
-﻿using Verse;
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
 
 namespace ArtificialBeings
 {
-    // Core Assistants do nothing special at the moment, but the class is used to make them mutually exclusive with one another.
+    // Core Assistant upgrades are destroyed if the pawn is dead and has no consciousness source.
     public class Hediff_CoreAssistant : Hediff
     {
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            if (pawn.health.hediffSet.GetNotMissingParts(tag: BodyPartTagDefOf.ConsciousnessSource).EnumerableNullOrEmpty())
+            {
+                if (pawn.Faction == Faction.OfPlayer && pawn.PositionHeld != null)
+                {
+                    MoteMaker.ThrowText(pawn.PositionHeld.ToVector3(), pawn.MapHeld, "ABF_ComponentDestroyed".Translate(LabelCap), Color.red, 10f);
+                }
+                pawn.health.RemoveHediff(this);
+                base.Notify_PawnDied(dinfo, culprit);
+            }
+        }
     }
 }
