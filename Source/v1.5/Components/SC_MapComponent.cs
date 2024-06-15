890dc015bee1b5f2f64aa79fc8cdf58cb7d09387
Killathon <huelva21@gmail.com>
2024-06-15 08:57:46 -0400

    Moved Recluse patch for drones to ABF

--- Source/v1.5/Components/SC_MapComponent.cs
+++ Source/v1.5/Components/SC_MapComponent.cs
@@ -4,8 +4,6 @@ namespace ArtificialBeings
 {
     public class SC_MapComponent : MapComponent
     {
-        public int playerDronesSpawned = 0;
-
         public SC_MapComponent(Map map) : base(map)
         {
         }
@@ -14,14 +12,6 @@ namespace ArtificialBeings
         {
             if (map.IsHashIntervalTick(GenTicks.TickLongInterval))
             {
-                playerDronesSpawned = 0;
-                for (int i = map.mapPawns.FreeColonistsAndPrisonersSpawnedCount - 1; i >= 0; i--)
-                {
-                    if (ABF_Utils.IsArtificialDrone(map.mapPawns.FreeColonistsAndPrisonersSpawned[i]))
-                    {
-                        playerDronesSpawned++;
-                    }
-                }
                 SC_Utils.UpdateAmicableDroneCount(map);
             }
         }
