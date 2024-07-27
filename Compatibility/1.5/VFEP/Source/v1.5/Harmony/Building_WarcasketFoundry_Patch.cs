8c7a6ac57eeb801cfc2b280e09048ae81d32629d
Killathon <huelva21@gmail.com>
2024-07-27 14:23:33 -0400

    Dewarcasketization completed

---
+++ Compatibility/1.5/VFEP/Source/v1.5/Harmony/Building_WarcasketFoundry_Patch.cs
@@ -0,0 +1,23 @@
+﻿using Verse;
+using HarmonyLib;
+using VFEPirates;
+
+namespace ArtificialBeings
+{
+    public class Building_WarcasketFoundry_Patch
+    {
+        // Synstructs cannot be entombed into warcaskets.
+        [HarmonyPatch(typeof(Building_WarcasketFoundry), nameof(Building_WarcasketFoundry.CannotUseNowReason))]
+        public class Building_WarcasketFoundry_CannotUseNowReason_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(ref string __result, Pawn selPawn)
+            {
+                if (__result == null && SC_Utils.IsSynstruct(selPawn))
+                {
+                    __result = "ABF_SynstructsCannotUseThis".Translate();
+                }
+            }
+        }
+    }
+}
---
+++ Compatibility/1.5/VFEP/Source/v1.5/SynstructsCore_VFEPCompatibility.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace ArtificialBeings
+{
+    [StaticConstructorOnStartup]
+    public static class SynstructsCore_VFEPCompatibility_PostInit
+    {
+        static SynstructsCore_VFEPCompatibility_PostInit()
+        {
+            new Harmony("SynstructsCore_VFEPCompatibility").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
