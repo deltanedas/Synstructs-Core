441c677e2846b4a348dec43434a7fcdc06583014
Killathon <huelva21@gmail.com>
2025-07-04 20:15:35 -0400

    Refactored/cached enervoir assignment code

--- Source/v1.6/Components/ThingComps/CompAssignableToPawn_EnergyReservoir.cs
+++ Source/v1.6/Components/ThingComps/CompAssignableToPawn_EnergyReservoir.cs
@@ -1,13 +1,41 @@
 ﻿using RimWorld;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Verse;
 
 namespace ArtificialBeings
 {
-    // A simple CompAssignable subclass that allows pawns to be assigned an energy reservoir. There are no restrictions besides being able to charge (no 1-1 restrictions, etc).
+    // A CompAssignable subclass that allows pawns to be assigned an energy reservoir. There are no restrictions besides being able to charge (no 1-1 restrictions, etc).
     public class CompAssignableToPawn_EnergyReservoir : CompAssignableToPawn
     {
+        private List<Pawn> cachedCandidates;
+
+        // Override the base class so that we can cache the assigning candidates when the dialog is opened, so we don't recalculate it every frame.
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            if (ShouldShowAssignmentGizmo())
+            {
+                Command_Action command_Action = new Command_Action
+                {
+                    defaultLabel = GetAssignmentGizmoLabel(),
+                    icon = ContentFinder<Texture2D>.Get("UI/Commands/AssignOwner"),
+                    defaultDesc = GetAssignmentGizmoDesc(),
+                    action = delegate
+                    {
+                        cachedCandidates?.Clear();
+                        Find.WindowStack.Add(new Dialog_AssignBuildingOwner(this));
+                    },
+                    hotKey = KeyBindingDefOf.Misc4
+                };
+                if (!Props.noAssignablePawnsDesc.NullOrEmpty() && !AssigningCandidates.Any())
+                {
+                    command_Action.Disable(Props.noAssignablePawnsDesc);
+                }
+                yield return command_Action;
+            }
+        }
+
         // All animals and colonists are possible candidates (with the exception of Dryads, which Core seems to think have a special use case).
         public override IEnumerable<Pawn> AssigningCandidates
         {
@@ -17,37 +45,46 @@ namespace ArtificialBeings
                 {
                     return Enumerable.Empty<Pawn>();
                 }
-                List<Pawn> candidates = new List<Pawn>();
-                foreach (Pawn pawn in parent.Map.mapPawns.SpawnedColonyAnimals)
+                if (cachedCandidates.NullOrEmpty())
                 {
-                    if (!pawn.RaceProps.Dryad)
+                    PawnComparer comparer = new PawnComparer();
+                    cachedCandidates = new List<Pawn>();
+                    foreach (Pawn pawn in parent.Map.mapPawns.SpawnedColonyAnimals)
                     {
-                        candidates.Add(pawn);
+                        if (SC_Utils.CanCharge(pawn))
+                        {
+                            cachedCandidates.InsertIntoSortedList(pawn, comparer);
+                        }
+                    }
+                    foreach (Pawn pawn in parent.Map.mapPawns.FreeColonists)
+                    {
+                        if (SC_Utils.CanCharge(pawn))
+                        {
+                            cachedCandidates.InsertIntoSortedList(pawn, comparer);
+                        }
                     }
                 }
-                foreach (Pawn pawn in parent.Map.mapPawns.FreeColonists)
-                {
-                    candidates.Add(pawn);
-                }
-                return candidates.OrderByDescending(delegate (Pawn p)
-                {
-                    if (!CanAssignTo(p).Accepted)
-                    {
-                        return 0;
-                    }
-                    return 1;
-                }).ThenBy((Pawn p) => p.LabelShort);
+                return cachedCandidates;
             }
         }
 
         // Can be assigned if the pawn can charge.
         public override AcceptanceReport CanAssignTo(Pawn pawn)
         {
-            if (SC_Utils.CanCharge(pawn))
+            if (!SC_Utils.CanCharge(pawn))
             {
-                return "ABF_IncapableOfCharging".Translate(pawn.LabelShortCap);
+                return "ABF_IncapableOfChargingShort".Translate();
             }
             return AcceptanceReport.WasAccepted;
+        }
+
+        // Comparer used to keep the list sorted as it is generated and cached.
+        private class PawnComparer : Comparer<Pawn>
+        {
+            public override int Compare(Pawn x, Pawn y)
+            {
+                return x.LabelShortCap.CompareTo(y.LabelShortCap);
+            }
         }
     }
 }
